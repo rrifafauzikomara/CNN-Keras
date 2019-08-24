b06461b9577e95da6991fcf62a7087d4df886fb0
rrifafauzikomara <rifafauzi6@gmail.com>
2019-08-24 17:00:24 +0700

    add fix error because of tab and space

--- cnn.py
+++ cnn.py
@@ -27,11 +27,11 @@ shear_range = 0.2,
 zoom_range = 0.2,
 horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('data/training_set',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('data/test_set',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
@@ -43,12 +43,12 @@ validation_steps = 2000)
 # Part 3 - Making new predictions
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('data/test_set/cats/cat.4007.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 training_set.class_indices
 if result[0][0] == 1:
-prediction = 'dog'
+    prediction = 'dog'
 else:
-prediction = 'cat'
+    prediction = 'cat'
